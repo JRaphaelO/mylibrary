1eb464759cd2c3abe06956e53e81ae03b073df9b
JRaphaelO <jraphal@eng.ci.ufpb.br>
2024-03-09 19:20:51 -0300

    feat: create books data

--- books/views.py
+++ books/views.py
@@ -1,3 +1,4 @@
+import requests
 from rest_framework import status, views
 from rest_framework.response import Response
 
@@ -15,5 +16,17 @@ class BookListView(views.APIView):
         serializer = BookSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
+
+            response = requests.get(f'https://www.googleapis.com/books/v1/volumes?q={serializer.data["title"]}')
+            data = response.json()
+            
+            for item in data['items']:
+                book = Book(
+                    title=item['volumeInfo']['title'],
+                    author=item['volumeInfo']['authors'][0],
+                    year_published=item['volumeInfo']['publishedDate']
+                )
+                book.save()
+
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
