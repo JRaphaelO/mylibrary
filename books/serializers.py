1fdd743ec678b722069b7ac57ef7f30bfd20030d
JRaphaelO <jraphal@eng.ci.ufpb.br>
2024-03-09 18:48:42 -0300

    refactor: change fields for all

--- books/serializers.py
+++ books/serializers.py
@@ -4,4 +4,4 @@ from .models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ['id', 'title', 'author', 'year_published']
+        fields = '__all__'
