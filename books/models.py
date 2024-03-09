c33b05aa82585301c4282b9726ec437b1dac08b6
JRaphaelO <jraphal@eng.ci.ufpb.br>
2024-03-09 18:45:43 -0300

    feat: create book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,13 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    year_published = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
