d96efd570dc4a0942499dff582a3fe14ea4b3188
JRaphaelO <jraphal@eng.ci.ufpb.br>
2024-03-09 18:51:15 -0300

    feat: create get route

--- books/views.py
+++ books/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django import views
+from rest_framework.response import Response
+
+from books.models import Book
+from books.serializers import BookSerializer
 
 # Create your views here.
+class BookListView(views.APIView):
+    def get(self, request, *args, **kwargs):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
